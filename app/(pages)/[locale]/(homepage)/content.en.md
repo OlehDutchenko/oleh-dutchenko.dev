1cb5e3516384d5293ac2ff1939ed82c9b49b87d0
OlehDutchenko <dutchenko.o.dev@gmail.com>
2023-12-13 07:19:41 +0200

    Update content for the Homepage

--- app/(pages)/[locale]/(homepage)/content.en.md
+++ app/(pages)/[locale]/(homepage)/content.en.md
@@ -1,3 +1,14 @@
-# Hello
+# Hello!<br> <small>My name is Oleg Dutchenko, and I'm a frontend developer!</small>
 
-This is the English version of the content.
+I like to work with React, TypeScript and GraphQL. I'm interested in the concept
+of "Clean Code" and practices of its
+implementation. Especially, I like "Test-Driven Development" (TDD), which I
+actively use in my tasks.
+
+On this site, I talk about my experience of commercial work as a tech lead and
+share my thoughts on technologies, practices and methodologies.
+
+You can read my [blog](/blog) or learn more [about me](/about).  
+Perhaps you will be interested in [the code I sell on Envato](/envato).
+
+I hope my experience, knowledge and services will be useful to you.
--- app/(pages)/[locale]/(homepage)/page.tsx
+++ app/(pages)/[locale]/(homepage)/page.tsx
@@ -2,6 +2,7 @@ import { Container } from '@/_components/Container';
 import { LocaleParam, STATIC_LOCALE_PARAMS } from '@/_locales/constants';
 import type { PageProps } from '@/_types/page-props';
 import { readMarkdownFile } from '@/_utils/readMarkdownFile';
+import Markdown from 'markdown-to-jsx';
 import React from 'react';
 
 export function generateStaticParams(): LocaleParam[] {
@@ -10,13 +11,10 @@ export function generateStaticParams(): LocaleParam[] {
 
 export default function Homepage({ params }: PageProps): React.ReactElement {
 	const markdown = readMarkdownFile(import.meta.url, params.locale);
-	console.log({ markdown });
 	return (
 		<main>
 			<Container>
-				<header>
-					<h1>Привіт світ!</h1>
-				</header>
+				<Markdown>{markdown}</Markdown>
 			</Container>
 		</main>
 	);
