976979cd250adacae4d83fac21416171202f6d5e
OlehDutchenko <dutchenko.o.dev@gmail.com>
2023-12-25 00:43:49 +0200

    Extends Vitest's method `expect` with new matchers

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,7 @@
-import { vi } from 'vitest';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from 'vitest';
+
+expect.extend(matchers);
 
 vi.mock('server-only', () => {
 	return {};
