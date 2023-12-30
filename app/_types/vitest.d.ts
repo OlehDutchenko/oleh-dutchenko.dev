3c1b354198d1494f93239e74fe889ce663cdf32d
OlehDutchenko <dutchenko.o.dev@gmail.com>
2023-12-30 12:00:04 +0200

    Fixes ESLint error

--- app/_types/vitest.d.ts
+++ app/_types/vitest.d.ts
@@ -4,6 +4,7 @@ import { type TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare global {
 	namespace jest {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/ban-types
 		interface Matchers<R = void, T = {}>
 			extends TestingLibraryMatchers<
 				ReturnType<typeof expect.stringContaining>,
