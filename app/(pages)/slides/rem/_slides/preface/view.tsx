7e605628f920d4efcd8a0dd6bf6d708a57e233c7
Oleh Dutchenko <dutchenko.o.dev@gmail.com>
2025-05-21 11:33:40 +0300

    Refactor slide view structure and update transition delays

--- app/(pages)/slides/rem/_slides/preface/view.tsx
+++ app/(pages)/slides/rem/_slides/preface/view.tsx
@@ -3,7 +3,7 @@ import { RemSlide } from '@slides/rem/_components/rem-slide';
 
 export function Preface() {
 	return (
-		<RemSlide bgColor="darker" width100p bgImage="wavesOfDarkWaterGif">
+		<RemSlide bgColor="darker" bgImage="wavesOfDarkWaterGif">
 			<div className="font-title">
 				<Fragment component="p" fx="fade-in-nth-child">
 					<span className="child child-auto">«У самому</span>
@@ -13,8 +13,10 @@ export function Preface() {
 					<span className="child child-auto">тисяч</span>
 					<span className="child child-auto">сторінок</span>
 					<span className="child child-auto">і екранів,</span>
-				</Fragment>
-				<Fragment component="p" fx="fade-in-nth-child">
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
 					<span className="child child-auto">браузери</span>
 					<span className="child child-auto">боронять</span>
 					<span className="child child-auto">право</span>
@@ -28,19 +30,17 @@ export function Preface() {
 					<span className="child child-auto">ця</span>
 					<span className="child child-auto">рівновага</span>
 					<span className="child child-auto">порушена.</span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto">Хтось…</span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto"></span>
+					<span className="child child-auto">використав</span>
+					<span className="child child-auto">
+						<code>px</code>»
+					</span>
 				</Fragment>
-				<p>
-					<Fragment fx="fade-in-nth-child">
-						<span className="child child-auto">Хтось…</span>
-						<span className="child child-auto"></span>
-						<span className="child child-auto"></span>
-						<span className="child child-auto"></span>
-						<span className="child child-auto">використав</span>
-						<span className="child child-auto">
-							<code>px</code>»
-						</span>
-					</Fragment>
-				</p>
 			</div>
 			<Fragment component="p" fx="fade-in-nth-child" className="t-right">
 				<span className="child child-auto">
