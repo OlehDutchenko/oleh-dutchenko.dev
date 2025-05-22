773f15382076de467a44ea28db19e1cbf7ccfd54
Oleh Dutchenko <dutchenko.o.dev@gmail.com>
2025-05-22 17:00:38 +0300

    WCAG Slide

--- app/(pages)/slides/rem/_slides/wcag/view.tsx
+++ app/(pages)/slides/rem/_slides/wcag/view.tsx
@@ -5,12 +5,7 @@ export function WCAG() {
 	return (
 		<>
 			<RemSlide transition="slide-in slide-out" bgColor="light">
-				<p>Але це не просто UX-нюанс</p>
-				<Fragment component="p">
-					Це порушення стандартів доступності,
-					<br />
-					зокрема — WCAG
-				</Fragment>
+				<p>Це про відповідність стандартам доступності</p>
 			</RemSlide>
 			<RemSlide transition="slide-in zoom-out" bgColor="light">
 				<h3>WCAG 2.2</h3>
@@ -55,6 +50,118 @@ export function WCAG() {
 						</small>
 					</p>
 				</blockquote>
+			</RemSlide>
+			<RemSlide transition="zoom-in slide-out" bgColor="lighter">
+				<p>
+					<b>Зміна масштабу вікна</b> <br /> та <br />{' '}
+					<b>зміна розміру шрифту</b> <br /> це <br />
+					<em>не одне й те саме</em>
+				</p>
+			</RemSlide>
+			<RemSlide transition="slide-in slide-out" bgColor="light">
+				<h3>
+					Механізми впливу <br /> на розмір тексту
+				</h3>
+			</RemSlide>
+			<RemSlide transition="slide-in slide-out" bgColor="light">
+				<p>1. Налаштування браузера</p>
+				<ul>
+					<li>
+						<b>Chrome</b> <br />{' '}
+						<small>
+							<small>Settings → Appearance → Font size</small>
+						</small>
+					</li>
+					<li>
+						<b>Firefox</b> <br />
+						<small>
+							<small>
+								Preferences → Language and Appearance → Fonts
+							</small>
+						</small>
+					</li>
+					<li>
+						<b>Safari</b> <br />
+						<small>
+							<code>⌥⌘+</code> <code>⌥⌘-</code>{' '}
+							<small>
+								<em>(працює лише для вкладки браузера)</em>
+							</small>
+						</small>
+					</li>
+				</ul>
+			</RemSlide>
+
+			<RemSlide transition="slide-in slide-out" bgColor="light">
+				<p>
+					2. Десктопні ОС <br />
+					<small>
+						<small>
+							<em>
+								Можуть впливати на певний перелік власних
+								програм
+							</em>
+						</small>
+					</small>
+				</p>
+				<ul>
+					<li>
+						<b>macOS</b> <br />{' '}
+						<small>
+							<small>
+								System Settings → Accessibility → Display → Text
+								size
+							</small>
+						</small>
+					</li>
+					<li>
+						<b>Windows</b> <br />
+						<small>
+							<small>Settings → Accessibility → Text size</small>
+						</small>
+					</li>
+					<li>
+						<b>Linux</b> <br />
+						<small>
+							<small>
+								Settings → Accessibility → Large Text <br />
+								<em>(залежить від білду)</em>
+							</small>
+						</small>
+					</li>
+				</ul>
+			</RemSlide>
+
+			<RemSlide transition="slide-in zoom-out" bgColor="light">
+				<p>3. Мобільні пристрої</p>
+				<p>
+					<small>
+						<small>
+							<em>
+								Браузери на мобільних пристроях, <br />{' '}
+								зазвичай, використовують параметри ОС
+							</em>
+						</small>
+					</small>
+				</p>
+				<ul>
+					<li>
+						<b>iOS / iPadOS </b> <br />{' '}
+						<small>
+							<small>
+								Settings → Accessibility → Display & Text Size
+							</small>
+						</small>
+					</li>
+					<li>
+						<b>Android</b> <br />
+						<small>
+							<small>
+								Settings → Display → Font size and display size
+							</small>
+						</small>
+					</li>
+				</ul>
 			</RemSlide>
 			<RemSlide transition="zoom-in zoom-out" bgColor="accent_rem">
 				<p>
